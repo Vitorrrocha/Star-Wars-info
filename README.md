f7d9ce3fe2906f947f5b25b6a0e1e7583832353f
vitorrrocha <vitorrochaflorip@hotmail.com>
2020-05-30 02:24:35 -0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Star Wars info
-### Uma aplicação web simples que lista alguns filmes da franquia Star Wars, feita em ReactJS.
---
+++ precache-manifest.e4731d9ff147601cc52338bd6201f58f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca34881d6ca2f0991769cca03cffb87",
+    "url": "/Star-Wars-info/index.html"
+  },
+  {
+    "revision": "637b071a4308da57344c",
+    "url": "/Star-Wars-info/static/css/main.8c61a513.chunk.css"
+  },
+  {
+    "revision": "d960e0511deda30c9102",
+    "url": "/Star-Wars-info/static/js/2.32486c63.chunk.js"
+  },
+  {
+    "revision": "73f631d49e0419a420c7f58042b4e6ae",
+    "url": "/Star-Wars-info/static/js/2.32486c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637b071a4308da57344c",
+    "url": "/Star-Wars-info/static/js/main.1eaae572.chunk.js"
+  },
+  {
+    "revision": "f184499f04b3039190b3",
+    "url": "/Star-Wars-info/static/js/runtime-main.853b76bd.js"
+  }
+]);
